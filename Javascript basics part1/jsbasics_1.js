aff69d7821b2ef4cbdb7c22d5cd6dec31af85787
Show-DR <showmiadr@gmail.com>
2023-05-10 16:33:36 +0530

    JAVASCRIPT BASICS

---
+++ Javascript basics part1/jsbasics_1.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    document.getElementById("demo2").innerHTML = "Paragraph changed to I am in Trichy";
+  }
+  document.write(6 + 6);
+  document.getElementById("demo").innerHTML = 8 + 8;
+  document.getElementById("demo1").innerHTML = 8 + 6;
+  window.alert("Welcome to our world");
+  console.log("How is it!");
+  
+  var  x = 10;
+  // Here x is 10
+  alert(x);
+  {  
+  var x = 2;
+  // Here x is 23
+  }
+  
+  // Here x is 2
+  document.getElementById("demo3").innerHTML = x;
+
